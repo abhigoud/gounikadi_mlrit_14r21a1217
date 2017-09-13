87dcdc04691a3038235251c1414cc4c66b358e46
abhigoud <31098627+abhigoud@users.noreply.github.com>
2017-09-13 13:52:13 +0530

    first

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+	public static void main(String[] args) {
+           Scanner in=new Scanner(System.in);
+       
+              n=in.nextInt();
+String student=new String[n];
+  for(i=0;i<n;i++)
+ {
+ student[i].id=in.nextInt();
+student[i]. fullName=in.nextLine();
+student[i].birthDate=in.nextInt();
+student[i].avgMark=in.nextDouble();
+  student obj=new student();
+obj.getId();
+obj.setId();
+obj.getFullName();
+obj.setFullName();
+obj.getBirthDate();
+obj.setBirthDate();
+obj.getAvgMark();
+obj.setAvgMark();
+obj.hashCode();
+
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
